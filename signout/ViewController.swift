c96064e3d8ff2b73ac9232b1b3030d1b131e12c3
Steve Kane <steve@stevekane.com.au>
2019-04-15 13:15:18 +1000

    Initial Commit

---
+++ signout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  signout
+//
+//  Created by Steve Kane on 15/4/19.
+//  Copyright © 2019 Steve Kane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
