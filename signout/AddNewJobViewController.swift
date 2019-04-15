e3510080702a3eebd53a01a29dd2120ecad61cfe
Steve Kane <steve@stevekane.com.au>
2019-04-15 14:00:18 +1000

    initial commit

---
+++ signout/AddNewJobViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewJobViewController.swift
+//  signout
+//
+//  Created by Steve Kane on 15/4/19.
+//  Copyright © 2019 Steve Kane. All rights reserved.
+//
+
+import UIKit
+
+class AddNewJobViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
